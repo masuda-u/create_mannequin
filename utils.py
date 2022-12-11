22073b9e0901099734153e76928c1379b437cee8
masuda-u <main.201809161230@gmail.com>
2022-12-11 12:33:24 +0900

    fix get_ellipse_another_radius

--- utils.py
+++ utils.py
@@ -21,19 +21,19 @@ def get_ellipse_another_radius(L,r):
     for i in range(10):
       k = k - F(k)/F_diff(k)
     # 離心率の式から短半径を求める
-    b = np.lib.scimath.sqrt(a**2*(1-k**2))
+    b = a*np.lib.scimath.sqrt(1-k**2)
     return b
   else:
     # rが短半径の場合
     b = r
     # 楕円の周の長さの公式
-    def F(k):return 4*np.lib.scimath.sqrt(b**2/(1-k**2))*sp.special.ellipe(k**2)-L 
+    def F(k):return 4*b/np.lib.scimath.sqrt(1-k**2)*sp.special.ellipe(k**2)-L 
     # F(k)の微分
     def F_diff(k):return (F(k+dk)-F(k-dk))/(2*dk)
     # ニュートン・ラプソン法で離心率を求める
     for i in range(10):
       k = k - F(k)/F_diff(k)
-    # 離心率の式から短半径を求める
-    a = np.lib.scimath.sqrt(b**2/(1-k**2))
+    # 離心率の式から長半径を求める
+    a = b/np.lib.scimath.sqrt(1-k**2)
     return a
   
